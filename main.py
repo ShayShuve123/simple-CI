994e8003d2d1c19a9523224455015e6135f0a340
Your Name <ShayShuve123>
2024-08-14 14:28:40 +0300

    Simple Ci files

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(number1,number2):
+    return number1+number2
+    
+ 
+def subtract(number1,number2):
+    return number1-number2
+
+
+def mult(number1,number2):
+    return number1*number2
+
+def div(number1,number2):
+    return number1/number2    
---
+++ test_main.py
@@ -0,0 +1,21 @@
+import unittest
+from main import add, subtract, mult, div
+
+class TestMain(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        
+    def test_subtract(self):
+        self.assertEqual(subtract(10,5), 5)
+        
+    def test_mult(self):
+        self.assertEqual(mult(3, 4), 12)
+
+    def test_div(self):
+        self.assertEqual(div(8, 4), 2)
+
+if __name__ == '__main__':
+    unittest.main()        
+        
+        
